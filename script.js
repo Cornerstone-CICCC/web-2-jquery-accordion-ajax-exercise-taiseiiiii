b967c0356bc682675e7997c2be93d5f15d96e0c7
taiseiiiii <isemiya.0509@gmail.com>
2025-11-26 22:38:18 -0800

    complete

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-$(function() {
-  // your code here
-})
+$(function () {
+  $(".accordion-header").on("click", function () {
+    const content = $(this).next(".accordion-content");
+    $(".accordion-content").not(content).slideUp();
+    content.slideToggle();
+  });
+
+  $("button").on("click", function () {
+    $.ajax({
+      url: "https://dummyjson.com/todos",
+      method: "GET",
+      success: function (data) {
+        const todos = data.todos;
+        const ul = $("ul");
+        ul.empty();
+
+        todos.forEach((item) => {
+          ul.append(`<li>${item.todo}</li>`);
+        });
+      },
+      error: function () {
+        alert("Failed");
+      },
+    });
+  });
+});
